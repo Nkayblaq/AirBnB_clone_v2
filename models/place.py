c2f09bfd9f433cefd39e6a70f6b3a400d2c385dd
chijioke <chijiokelonginus4@gmail.com>
2024-04-18 10:56:11 +0100

    updated

--- models/place.py
+++ models/place.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import Base, BaseModel
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
